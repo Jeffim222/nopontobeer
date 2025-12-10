781d741bf7fdd43164e23f58f283f4f00668ba56
jeffim222 <jeffersonalves1993@gmail.com>
2025-12-10 10:29:02 -0300

    terceiro commit

--- main.py
+++ main.py
@@ -3,14 +3,11 @@ from db import get_connection
 import os
 
 app = Flask(__name__)
-
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 
 @app.route("/")
 def index():
     return send_from_directory(BASE_DIR, "index.html")
-
 
 @app.route("/inserir", methods=["POST"])
 def inserir():
@@ -18,20 +15,19 @@ def inserir():
 
     conn = get_connection()
     cur = conn.cursor()
-    cur.execute("INSERT INTO teste (nome) VALUES (%s)", (nome,))
+
+    cur.execute("INSERT INTO clientes (nome) VALUES (%s)", (nome,))
     conn.commit()
 
     cur.close()
     conn.close()
 
-    return "Inserido com sucesso!"
+    return f"Nome {nome} inserido com sucesso!"
 
-
-# Isso permite acessar CSS, JS, imagens na raiz
+# Permite acessar arquivos estáticos da raiz
 @app.route("/<path:filename>")
 def static_files(filename):
     return send_from_directory(BASE_DIR, filename)
-
 
 if __name__ == "__main__":
     app.run()
